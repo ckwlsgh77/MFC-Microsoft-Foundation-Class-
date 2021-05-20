59acbbcbfdf98a16fa59482807b0685fe63b20cb
jinho <ckwlsgh77777@naver.com>
2021-05-20 15:41:51 +0900

    DB Close when exiting

--- Diary/DiaryView.cpp
+++ Diary/DiaryView.cpp
@@ -709,7 +709,11 @@ void CDiaryView::OnButtonBlue()
 void CDiaryView::OnClose()
 {
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_command.Close();
+	m_dbSet.~CDataSet();
 	CFormView::OnClose();
-	exit(1);
+	CMainFrame *pFrame = (CMainFrame *)AfxGetMainWnd();
+	pFrame->OnClose();
+	//exit(1);
 
 }
--- Diary/LoginDlg.cpp
+++ Diary/LoginDlg.cpp
@@ -61,7 +61,8 @@ void LoginDlg::OnClose()
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 	CDialogEx::OnClose();
 	CMainFrame *pFrame = (CMainFrame *)AfxGetMainWnd();
-	pFrame->OnClose();
+	CDiaryView *pView = (CDiaryView *)pFrame->GetActiveView();
+	pView->OnClose();
 	//CDiaryView *pView=(CDiaryView *)pFrame->GetActiveView();;
 	//pView->OnClose();
 }
